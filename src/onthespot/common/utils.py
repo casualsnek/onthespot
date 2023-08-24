d1cfafa45881a7e3418a787be8bf76cea36a9b37
Casu Al Snek <casualsnek@pm.me>
2023-08-24 23:56:02 +0545

    Fix formatting

--- src/onthespot/common/utils.py
+++ src/onthespot/common/utils.py
@@ -8,6 +8,41 @@ import time
 from ..common.formating import sanitize_string
 from typing import Union
 
+GENRES = {
+    "acoustic", "afrobeat", "alt-rock", "alternative",
+    "ambient", "anime", "black-metal", "bluegrass",
+    "blues", "bossanova", "brazil", "breakbeat",
+    "british", "cantopop", "chicago-house", "children",
+    "chill", "classical", "club", "comedy",
+    "country", "dance", "dancehall", "death-metal",
+    "deep-house", "detroit-techno", "disco", "disney",
+    "drum-and-bass", "dub", "dubstep", "edm",
+    "electro", "electronic", "emo", "folk",
+    "forro", "french", "funk", "garage",
+    "german", "gospel", "goth", "grindcore",
+    "groove", "grunge", "guitar", "happy",
+    "hard-rock", "hardcore", "hardstyle", "heavy-metal",
+    "hip-hop", "holidays", "honky-tonk",
+    "house", "idm", "indian", "indie",
+    "indie-pop", "industrial", "iranian", "j-dance",
+    "j-idol", "j-pop", "j-rock", "jazz",
+    "k-pop", "kids", "latin", "latino",
+    "malay", "mandopop", "metal", "metal-misc",
+    "metalcore", "minimal-techno", "movies", "mpb",
+    "new-age", "new-release", "opera", "pagode",
+    "party", "philippines-opm", "piano", "pop",
+    "pop-film", "post-dubstep", "power-pop", "progressive-house",
+    "psych-rock", "punk", "punk-rock", "r-n-b",
+    "rainy-day", "reggae", "reggaeton", "road-trip",
+    "rock", "rock-n-roll", "rockabilly",
+    "romance", "sad", "salsa", "samba",
+    "sertanejo", "show-tunes", "singer-songwriter",
+    "ska", "sleep", "songwriter", "soul",
+    "soundtracks", "spanish", "study", "summer",
+    "swedish", "synth-pop", "tango", "techno",
+    "trance", "trip-hop", "turkish", "work-out", "world-music"
+}
+
 
 def cached_request(cache_dir: Union[str, None], lifetime: int, *args, **kwargs) -> str:
     """
@@ -127,6 +162,7 @@ def pick_thumbnail(covers: list[dict], preferred_size: int = 640000) -> str:
         if size >= preferred_size:
             return images[size]
     return images[available_sizes[-1]] if len(available_sizes) > 0 else ""
+
 
 class MutableBool:
     def __init__(self, value: bool = False):
