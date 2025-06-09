e840e55b6071365118fb6dfe2cc4c83666f8fb64
Casu Al Snek <casualsnek@pm.me>
2025-06-09 18:11:40 +0545

    Add skeleton for jobs and downloader

---
+++ src/onthespot/job.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from typing import Callable, Any, List
+from lmttfy import invoke_in_thread, ThreadedCall
+from .services.sessions import SessionsService
+from otslib.core.mediaitem import SpotifyTrackMedia, SpotifyEpisodeMedia, SpotifyAlbum, SpotifyPodcast, SpotifyPlaylist, SpotifyArtist
+from otslib.common.url import
+class DownloadStatus(Enum):
+    Pending = 0
+    Downloading = 1
+    Error = 2
+    Success = 3
+    Cancelled = 4
+
+# This needs a lot of work
+class MediaDownloadJob:
+    def __init__(self, media_uuid: str, media_type: SpotifyTrackMedia|SpotifyEpisodeMedia,
+                 on_progress: Callable,
+                 on_complete: Callable,
+                 on_error: Callable,
+                 on_success: Callable
+                 ):
+        # Media metadata
+        self.__media_uuid = media_uuid
+        self.__media_type: SpotifyTrackMedia|SpotifyEpisodeMedia = media_type
+
+        # Download state
+        self.__bytes_downloaded = 0
+        self.__bytes_total = 0
+        self.__download_path = None
+        self.__status = DownloadStatus.Pending
+        self.__status_text = "Pending"
+
+        # Event handlers
+        self.__on_complete = on_complete  # When both download and transcoding are done
+        self.__on_progress = on_progress  # When download progress is updated
+        self.__on_error = on_error  # When download fails
+        self.__on_success = on_success  # When download succeeds and transcoding begins
+
+    @invoke_in_thread(max_concurrent_execs=1)
+    def fetch_media(self, session_service: SessionsService) -> None|ThreadedCall:
+        pass
+
+class CollectionsDownloadJobMaker:
+    def __init__(self, media_uuid: str, media_type: int,
+                 on_progress: Callable,
+                 on_complete: Callable,
+                 on_error: Callable,
+                 on_success: Callable,
+                 ):
+        self.__media_uuid = media_uuid
+        self.__media_type = media_type
+
+        # Event handlers
+        self.__on_complete = on_complete  # When both download and transcoding are done
+        self.__on_progress = on_progress  # When download progress is updated
+        self.__on_error = on_error  # When download fails
+        self.__on_success = on_success  # When download succeeds and transcoding begins
+
+
+    @invoke_in_thread(max_concurrent_execs=1)
+    def make_jobs(self, parsing_session: Any) -> List[MediaDownloadJob]: # Boo hoo, scary
+        # Begin parsing
+        medias: List[MediaDownloadJob] = []
+        return medias
---
+++ src/onthespot/services/downloader.py
@@ -0,0 +1,39 @@
+from collections.abc import Callable
+from threading import Lock
+from typing import List, Any
+from enum import Enum
+from .configuration import ConfigurationService
+from .sessions import SessionsService
+from queue import Queue
+from lmttfy.thread import invoke_in_thread, ThreadedCall
+from ..job import MediaDownloadJob
+
+
+class DownloaderService:
+    def __init__(self, config_service: ConfigurationService, sessions_service: SessionsService):
+        self.__config_service = config_service
+        self.__sessions_service = sessions_service
+        self.__queue: Queue[MediaDownloadJob] = Queue()
+        self.__threads = []
+        self.__lock = Lock()
+
+    @invoke_in_thread(max_concurrent_execs=1)
+    def start(self):
+
+        while True:
+            if len(self.__threads) <= self.__config_service.get("max_concurrent_downloads"):
+                job: MediaDownloadJob = self.__queue.get()
+                job.fetch_media(self.__sessions_service).on_complete(lambda ret: self.__remove_thread(job)).on_error(
+                    lambda e: self.__remove_thread(job)
+                )
+                with self.__lock:
+                    self.__threads.append(job)
+
+    def __remove_thread(self, job: MediaDownloadJob):
+        with self.__lock:
+            self.__threads.remove(job)
+
+
+    def add_job(self, job: MediaDownloadJob):
+        self.__queue.put(job)
+
